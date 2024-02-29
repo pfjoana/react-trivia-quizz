9141ce965255bdaaf0efcaf401e2946cf040a725
joana faria <paisdefaria.joana@gmail.com>
2024-02-29 19:00:27 +0000

    installed he lib, basic screen structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
+import he from 'he'
 import './App.css'
 
 export default function App() {
 
+  const [start, setStart] = useState(true)
+  const [check, setCheck] = useState(false)
+
+  // const quizzElements =
+
   return (
     <>
+      { start ?
+      <div className="start-screen">
+        <h1>A great Trivia Quizz</h1>
+        <button className="main-button" onClick={() => setStart(false)}>Start</button>
+      </div>
+      :
+      <div>
+        <button className="main-button" onClick={() => setCheck(true)}>Check answers</button>
+      </div>
+      }
     </>
   )
 }
-
