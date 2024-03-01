26db58ac6b25484a3f04302ab0d4d039f9e75f81
joana faria <paisdefaria.joana@gmail.com>
2024-03-01 11:56:41 +0000

    API called and data saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import he from 'he'
 import './App.css'
 
 export default function App() {
 
-  const [start, setStart] = useState(true)
-  const [check, setCheck] = useState(false)
+  const [startScreen, setStartScreen] = useState(true)
+  const [check, setCheck] = useState(false) // when check is true verify answer, add this later
 
-  // const quizzElements =
+  //empty QA to insert decoded data
+  // const [QA, setQA] = useState([])
+
+
+  // store data from API
+  const [allQA, setAllQA] = useState([])
+
+  // call API
+  useEffect(()=> {
+    console.log("Effect runned")
+    fetch("https://opentdb.com/api.php?amount=10")
+    .then(data => data.json())
+    .then(data => setAllQA(data.results))
+  }, []
+  )
+
+  console.log(allQA)
+
 
   return (
     <>
-      { start ?
+      { startScreen ?
       <div className="start-screen">
         <h1>A great Trivia Quizz</h1>
-        <button className="main-button" onClick={() => setStart(false)}>Start</button>
+        <button className="main-button" onClick={() => setStartScreen(false)}>Start</button>
       </div>
       :
       <div>
+        {/* add QA elements from component */}
         <button className="main-button" onClick={() => setCheck(true)}>Check answers</button>
       </div>
       }
